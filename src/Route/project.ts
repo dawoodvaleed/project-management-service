82bf5f84a05e9523324ab8221a224742af830c3c
dawoodvaleed <dawoodvaleed@yahoo.com>
2024-04-21 17:52:05 +0200

    remove token verification form project routes

--- src/Route/project.ts
+++ src/Route/project.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { addProject, getProjects } from "../Controller";
-import { validateToken } from "../Middleware";
+// import { validateToken } from "../Middleware";
 
 export const projectRouter = Router();
 
-projectRouter.get("/", validateToken, getProjects);
+// TODO: replace the bottom ones with the validated routes
+
+// projectRouter.get("/", validateToken, getProjects);
+// projectRouter.post("/", validateToken, addProject);
+
+projectRouter.get("/", getProjects);
 projectRouter.post("/", addProject);
