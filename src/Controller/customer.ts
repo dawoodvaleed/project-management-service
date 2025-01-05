c459b5c05a0ed4a2adf5e70aeb953864cde3be7d
dawoodvaleed <dawoodvaleed@yahoo.com>
2025-01-05 19:42:00 +0100

    add update endpoint for customer

--- src/Controller/customer.ts
+++ src/Controller/customer.ts
@@ -20,3 +20,71 @@ export const getCustomers = async (req: Request, res: Response) => {
     console.error(error);
   }
 };
+
+export const updateCustomer = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const {
+      name,
+      bankAccountTitle,
+      bankAccountNumber,
+      contactPerson,
+      landline,
+      mobile,
+      cnic,
+      fax,
+      ntn,
+      strn,
+      email,
+      province,
+      raServiceTax,
+      bankHoldTax,
+      incomeTax,
+      address,
+      city,
+      shortBillGenerationLimit,
+      isActive,
+      advancePercentage,
+      firstRunningPercentage,
+      secondRunningPercentage,
+    } = req.body;
+
+    const data = {
+      name,
+      bankAccountTitle,
+      bankAccountNumber,
+      contactPerson,
+      landline,
+      mobile,
+      cnic,
+      fax,
+      ntn,
+      strn,
+      email,
+      province,
+      raServiceTax,
+      bankHoldTax,
+      incomeTax,
+      address,
+      city,
+      shortBillGenerationLimit,
+      isActive,
+      advancePercentage,
+      firstRunningPercentage,
+      secondRunningPercentage,
+    };
+
+    if (id) {
+      const response = await customerRepository.update(id, data);
+      if (response) {
+        return res.status(200).send("Customer updated");
+      } else {
+        return res.status(404).send("Customer does not exist");
+      }
+    } else {
+      res.status(500).send("Failed to update customer");
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
--- src/Route/customer.ts
+++ src/Route/customer.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { getCustomers } from "../Controller";
+import { getCustomers, updateCustomer } from "../Controller";
 import { validateToken } from "../Middleware";
 
 export const customerRouter = Router();
 
 customerRouter.get("/", validateToken, getCustomers);
+customerRouter.put("/:id", validateToken, updateCustomer);
