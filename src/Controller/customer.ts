7743cfb92ede0d4f6cd29dcee98aaa4238ff8c25
dawoodvaleed <dawoodvaleed@gmail.com>
2025-04-20 12:19:15 +0200

    add customer stats endpoint

--- src/Controller/customer.ts
+++ src/Controller/customer.ts
@@ -88,3 +88,29 @@ export const updateCustomer = async (req: Request, res: Response) => {
     console.error(error);
   }
 };
+
+export const getCustomersStats = async (req: Request, res: Response) => {
+  try {
+    const { offset = 0, limit = 10 } = req.query;
+
+    const [customers, totalCustomers] = await Promise.all([
+      customerRepository
+      .createQueryBuilder("customer")
+      .leftJoinAndSelect("customer.projects", "project")
+      .select("customer.id", "customerId")
+      .addSelect("customer.name", "customerName")
+      .addSelect("SUM(CASE WHEN project.type = 'NEW' THEN 1 ELSE 0 END)", "newProjectsCount")
+      .addSelect("SUM(CASE WHEN project.type = 'MAINTENANCE' THEN 1 ELSE 0 END)", "maintenanceProjectsCount")
+      .groupBy("customer.id")
+      .addGroupBy("customer.name")
+      .offset(Number(offset))
+      .limit(Number(limit))
+      .getRawMany(),
+      customerRepository.count(),
+    ]);
+
+    return res.status(200).send([customers, totalCustomers]);
+  } catch (error) {
+    console.error(error);
+  }
+}
--- src/Route/customer.ts
+++ src/Route/customer.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getCustomers, updateCustomer } from "../Controller";
+import { getCustomers, getCustomersStats, updateCustomer } from "../Controller";
 import { validateToken } from "../Middleware";
 
 export const customerRouter = Router();
 
 customerRouter.get("/", validateToken, getCustomers);
+customerRouter.get("/stats",validateToken, getCustomersStats);
 customerRouter.put("/:id", validateToken, updateCustomer);
